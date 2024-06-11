f21533236234271de8c3728cad92b9cd1db60c8c
Angie Carolina <33725657+angiecarojas@users.noreply.github.com>
2024-06-10 23:38:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ st.write("The name on your Smoothie will be:", name_on_order)
 try:
     cnx = st.experimental_connection("snowflake")
     session = cnx.session()
+    # Cambiar el warehouse a uno con créditos disponibles
+    session.sql("USE WAREHOUSE MY_ALTERNATE_WAREHOUSE").collect()
     st.write("Conexión a Snowflake establecida correctamente.")
 except Exception as e:
     st.error(f"Error estableciendo la conexión a Snowflake: {e}")
@@ -54,7 +56,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
@@ -76,6 +78,7 @@ if ingredients_list:
             st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         except Exception as e:
             st.error(f"Error al insertar la orden en Snowflake: {e}")
+
 
     
 
